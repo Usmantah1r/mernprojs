87f88c19602eaadb04a9e6ad0e39d7f524febfdd
Usmantah1r <usmantahir980.1@gmail.com>
2026-05-12 15:01:24 +0500

    Translation App

---
+++ Translatorr/countries.js
@@ -0,0 +1,178 @@
+const countries = {
+  "af-ZA": "Afrikaans",
+  "am-ET": "Amharic",
+
+  "ar-AE": "Arabic (UAE)",
+  "ar-DZ": "Arabic (Algeria)",
+  "ar-EG": "Arabic (Egypt)",
+  "ar-IQ": "Arabic (Iraq)",
+  "ar-JO": "Arabic (Jordan)",
+  "ar-KW": "Arabic (Kuwait)",
+  "ar-LB": "Arabic (Lebanon)",
+  "ar-MA": "Arabic (Morocco)",
+  "ar-OM": "Arabic (Oman)",
+  "ar-QA": "Arabic (Qatar)",
+  "ar-SA": "Arabic (SA)",
+  "ar-TN": "Arabic (Tunisia)",
+
+  "hy-AM": "Armenian",
+
+  "bn-BD": "Bengali",
+
+  "my-MM": "Burmese",
+
+  "zh-CN": "Chinese (China)",
+
+  "hr-HR": "Croatian",
+
+  "cs-CZ": "Czech",
+
+  "da-DK": "Danish",
+
+  "fa-AF": "Dari (Afghanistan)",
+
+  "nl-BE": "Dutch (Belgium)",
+  "nl-NL": "Dutch (Netherlands)",
+
+  "en-AU": "English (Australia)",
+  "en-CA": "English (Canada)",
+  "en-GH": "English (Ghana)",
+  "en-NG": "English (Nigeria)",
+  "en-NZ": "English (NZ)",
+  "en-GB": "English (UK)",
+  "en-US": "English (US)",
+
+  "et-EE": "Estonian",
+
+  "fi-FI": "Finnish",
+
+  "fr-BE": "French (Belgium)",
+  "fr-CM": "French (Cameroon)",
+  "fr-CA": "French (Canada)",
+  "fr-FR": "French (France)",
+  "fr-CI": "French (Ivory Coast)",
+  "fr-SN": "French (Senegal)",
+  "fr-CH": "French (Switzerland)",
+
+  "ka-GE": "Georgian",
+
+  "de-AT": "German (Austria)",
+  "de-DE": "German (Germany)",
+  "de-CH": "German (Switzerland)",
+
+  "el-GR": "Greek",
+
+  "gu-IN": "Gujarati",
+
+  "he-IL": "Hebrew",
+
+  "hi-IN": "Hindi",
+
+  "hu-HU": "Hungarian",
+
+  "is-IS": "Icelandic",
+
+  "id-ID": "Indonesian",
+
+  "ga-IE": "Irish",
+
+  "it-IT": "Italian (Italy)",
+  "it-CH": "Italian (Switzerland)",
+
+  "ja-JP": "Japanese",
+
+  "kn-IN": "Kannada",
+
+  "kk-KZ": "Kazakh",
+
+  "km-KH": "Khmer",
+
+  "ko-KR": "Korean",
+
+  "lo-LA": "Lao",
+
+  "lv-LV": "Latvian",
+
+  "lt-LT": "Lithuanian",
+
+  "ms-MY": "Malay",
+
+  "ml-IN": "Malayalam",
+
+  "mr-IN": "Marathi",
+
+  "mn-MN": "Mongolian",
+
+  "ne-NP": "Nepali",
+
+  "no-NO": "Norwegian",
+
+  "fa-IR": "Persian (Iran)",
+
+  "ps-AF": "Pashto (Afghanistan)",
+
+  "pl-PL": "Polish",
+
+  "pt-BR": "Portuguese (Brazil)",
+  "pt-PT": "Portuguese (Portugal)",
+
+  "pa-IN": "Punjabi (India)",
+
+  "ro-RO": "Romanian",
+
+  "ru-RU": "Russian",
+
+  "sr-RS": "Serbian",
+
+  "si-LK": "Sinhala",
+
+  "sk-SK": "Slovak",
+
+  "sl-SI": "Slovenian",
+
+  "es-AR": "Spanish (Argentina)",
+  "es-BO": "Spanish (Bolivia)",
+  "es-CL": "Spanish (Chile)",
+  "es-CO": "Spanish (Colombia)",
+  "es-CR": "Spanish (Costa Rica)",
+  "es-DO": "Spanish (Dominican Republic)",
+  "es-EC": "Spanish (Ecuador)",
+  "es-SV": "Spanish (El Salvador)",
+  "es-GT": "Spanish (Guatemala)",
+  "es-HN": "Spanish (Honduras)",
+  "es-MX": "Spanish (Mexico)",
+  "es-NI": "Spanish (Nicaragua)",
+  "es-PA": "Spanish (Panama)",
+  "es-PY": "Spanish (Paraguay)",
+  "es-PE": "Spanish (Peru)",
+  "es-ES": "Spanish (Spain)",
+  "es-UY": "Spanish (Uruguay)",
+  "es-VE": "Spanish (Venezuela)",
+
+  "sw-KE": "Swahili (Kenya)",
+  "sw-TZ": "Swahili (Tanzania)",
+
+  "sv-SE": "Swedish",
+
+  "tl-PH": "Tagalog",
+
+  "ta-IN": "Tamil",
+
+  "te-IN": "Telugu",
+
+  "th-TH": "Thai",
+
+  "tr-TR": "Turkish",
+
+  "uk-UA": "Ukrainian",
+
+  "ur-PK": "Urdu",
+
+  "uz-UZ": "Uzbek",
+
+  "vi-VN": "Vietnamese",
+
+  "cy-GB": "Welsh",
+
+  "zu-ZA": "Zulu"
+};
---
+++ Translatorr/script.js
@@ -0,0 +1,57 @@
+const selectTag=document.querySelectorAll("select");
+const translateBtn = document.querySelector("#translate");
+const fromText = document.querySelector("#fromtext");
+const toText = document.querySelector("#totext");
+const icons = document.querySelectorAll("img");
+
+selectTag.forEach((tag, id)=>{
+
+    for (const countriesCode in countries){
+        let selected;
+        if(id==0 && countriesCode=="en-US"){
+            selected="selected";
+        }else if(id==1 && countriesCode=="ja-JP"){
+            selected="selected";
+        }
+
+      let option = `<option value="${countriesCode}" ${selected}>${countries[countriesCode]}</option>`;
+      tag.insertAdjacentHTML("beforeend", option);
+    }
+});
+
+translateBtn.addEventListener("click",()=>{
+    let Text=fromText.value,
+    translateFrom=selectTag[0].value,
+    translateTo=selectTag[1].value;
+
+
+    let apiURL=`https://api.mymemory.translated.net/get?q=${Text}!&langpair=${translateFrom}|${translateTo}`;
+
+    fetch(apiURL).then(res=>res.json()).then(data=>{
+        toText.value=data.responseData.translatedText;
+    });
+});
+icons.forEach(icon =>{
+    icon.addEventListener("click",({target})=>{
+        if(target.classList.contains("copy")){
+            if(target.id=="from"){
+                navigator.clipboard.writeText(fromText.value)
+            }else{
+                navigator.clipboard.writeText(toText.value)
+            }
+        }
+        else{
+            let utterance;
+            if(target.id=="from"){
+                utterance = new SpeechSynthesisUtterance(fromText.value)
+                utterance.lang=selectTag[0].value
+            }
+            else{
+                utterance = new SpeechSynthesisUtterance(toText.value)
+                utterance.lang=selectTag[1].value
+            }
+            speechSynthesis.speak(utterance);
+        }
+    });
+});
+    
